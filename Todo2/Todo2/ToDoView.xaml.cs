03a89fb6ee890a462de03a19e07cc1f5720208f9
Sajidahmed Reshmi <sajidreshmi@gmail.com>
2022-03-03 23:24:08 +0100

    first commit

---
+++ Todo2/Todo2/ToDoView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoView : ContentPage
+    {
+        public ToDoView()
+        {
+            InitializeComponent();
+            BindingContext = new ToDoViewModel();
+        }
+    }
+}
---
+++ Todo2/Todo2/ToDoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Todo2
+{
+    internal class ToDoViewModel: BindableObject
+    {
+        private ToDoItem _selectedItem;
+        public ToDoViewModel()
+        {
+            Items = new ObservableCollection<ToDoItem>(ToDoItem.GetToDoItems());
+        }
+
+        public ObservableCollection<ToDoItem> Items { get; set; }
+
+        public String PageTitle { get; set; }
+
+        public ToDoItem SelectedItem { get => _selectedItem;
+            set {
+                _selectedItem = value;
+                PageTitle = value?.Name;
+                OnPropertyChanged("PageTitle");
+            } 
+        }
+    }
+}
