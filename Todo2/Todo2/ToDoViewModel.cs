b87754db8aa58fd2bd6201bf85626b0b34ba1aae
Sajidahmed Reshmi <sajidreshmi@gmail.com>
2022-03-05 14:35:16 +0100

    todo view page

--- Todo2/Todo2/ToDoViewModel.cs
+++ Todo2/Todo2/ToDoViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
+using System.Windows.Input;
 
 namespace Todo2
 {
@@ -24,6 +25,24 @@ namespace Todo2
                 PageTitle = value?.Name;
                 OnPropertyChanged("PageTitle");
             } 
+        }
+
+        public ICommand AddItemCommand => new Command(() =>
+        {
+            AddNewItem();
+        });
+
+        public ICommand MarkAsCompletedCommand => new Command<ToDoItem>(MarkAsCompleted);
+
+        private void MarkAsCompleted(ToDoItem obj)
+        {
+            obj.Completed = true;
+        }
+
+        private void AddNewItem()
+        {
+            Items.
+               Add(new ToDoItem($"Todo Item {Items.Count + 1}"));
         }
     }
 }
--- Todo2/Todo2/obj/Debug/netstandard2.0/ToDoView.xaml.g.cs
+++ Todo2/Todo2/obj/Debug/netstandard2.0/ToDoView.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Todo2 {
     public partial class ToDoView : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ContentPage ToDoViewPage;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ToDoView));
+            ToDoViewPage = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ContentPage>(this, "ToDoViewPage");
         }
     }
 }
